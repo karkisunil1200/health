124bc541a87ef753a5bae9425859af39f1707d97
Sunil Karki <karkisunil1200@gmail.com>
2020-01-24 14:38:19 -0600

    Added redux and created store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import App from './App';
+import {reducer} from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
